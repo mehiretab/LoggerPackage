37f425a2301f49815275ef5df2734ffb9f26afaa
Mehiretab Teshome <mehiretab333@gmail.com>
2023-01-04 07:20:40 +0300

    Initial commit

---
+++ Sources/LoggerPackage/Logger.swift
@@ -0,0 +1,15 @@
+//
+//  Logger.swift
+//  TestLogger
+//
+//  Created by Mehiretab on 04/01/2023.
+//
+
+import Foundation
+
+
+public class Logger {
+    public static func printMessage(message: String) {
+        print("Logger: ==== \(message)")
+    }
+}
--- Sources/LoggerPackage/LoggerPackage.swift
+++
@@ -1,6 +0,0 @@
-public struct LoggerPackage {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
